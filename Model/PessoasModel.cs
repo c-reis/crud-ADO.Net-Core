2bc917226e11b63070f15781f94c6e623bc6fab2
Carlos Reis <carlosreis11@hotmail.com>
2020-12-16 11:40:37 -0300

    Alterações em PessoasModel e ProdutosModel

--- Model/PessoasModel.cs
+++ Model/PessoasModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,9 +8,13 @@ namespace crud_cadastro.Model
 {
     public class PessoasModel
     {
+        [Key]
         public Guid Pessoas_id { get; set; }
+        [Required]
         public string Nome { get; set; }
+        [Required]
         public string Endereco { get; set; }
+        [Required]
         public string Cidade { get; set; }
 
     }
--- Model/ProdutosModel.cs
+++ Model/ProdutosModel.cs
@@ -8,10 +8,13 @@ namespace crud_cadastro.Model
 {
     public class ProdutosModel
     {
+        [Key]
         public Guid Produtos_id { get; set; }
+        [Required]
         public string Descricao { get; set; }
-        [Range(0,99999.99)]
+        [Required, Range(0,99999.99)]
         public double Preco { get; set; }
+        [Required]
         public int Quantidade { get; set; }
     }
 }
